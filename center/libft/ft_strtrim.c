ed6af251735cdae25d6a2b0aa26ac36cca78728d
Oguzhan Oksuz <ooksuz@k1m33s05.42istanbul.com.tr>
2022-10-14 16:25:05 +0300

    split

--- center/libft/ft_strtrim.c
+++ center/libft/ft_strtrim.c
@@ -33,9 +33,11 @@ int	ft_trimlen (const char *s1, const char *set)
 
 	j = 0;
 	i = 0;
-	while (ft_isinset(s1[i], set))
+	while (ft_isinset(s1[i], set) && s1[i])
 		i++;
-	while (ft_isinset(s1[ft_strlen(s1) - j], set))
+	if (i == ft_strlen(s1))
+		return (i);
+	while (ft_isinset(s1[ft_strlen(s1) - j - 1], set))
 		j++;
 	return (i + j);
 }
@@ -50,13 +52,19 @@ char	*ft_strtrim(const char *s1, char const *set)
 	i = 0;
 	j = 0;
 	len = 0;
+	if (!set)
+		return (ft_strdup(s1));
+	if (!s1)
+		return (0);
 	while (s1[len])
 		len++;
 	len = len - ft_trimlen(s1, set) + 1;
 	str = malloc(sizeof(char) * len);
+	if (!str)
+		return (0);
 	while (ft_isinset(s1[i], set))
 		i++;
-	while (j < len)
+	while (j < len - 1)
 		str[j++] = s1[i++];
 	str[j] = 0;
 	return (str);
