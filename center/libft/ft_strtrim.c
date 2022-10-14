117357207879398ae80fdaac447173644655958d
Oguzhan Oksuz <ooksuz@k1m32s10.42istanbul.com.tr>
2022-10-14 14:06:16 +0300

    ft_trim

--- center/libft/ft_strtrim.c
+++ center/libft/ft_strtrim.c
@@ -14,51 +14,50 @@
 
 int	ft_isinset(const char c, const char *set)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
 	while (set[i])
 	{
-		if (set[i] == c)
+		if (c == set[i])
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-int	ft_trimmedlen(const char *s1, char const *set)
-{
-	size_t	i;
-	size_t	len;
-
-	len = 0;
-	i = 0;
-	while (s1[i] && !ft_isinset(s1[i], set))
-	{
-		if (!ft_isinset(s1[i], set))
-			len++;
-		i++;
-	}
-	return (len + 1);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
+int	ft_trimlen (const char *s1, const char *set)
 {
 	size_t	i;
 	size_t	j;
-	char	*str;
 
 	j = 0;
 	i = 0;
-	str = malloc(sizeof(char) * ft_trimmedlen(s1, set));
-	if (!str)
-		return (0);
-	while (s1[i])
-	{
-		if (!ft_isinset(s1[i], set))
-			str[j++] = s1[i];
+	while (ft_isinset(s1[i], set))
 		i++;
-	}
-	str[i] = 0;
+	while (ft_isinset(s1[ft_strlen(s1) - j], set))
+		j++;
+	return (i + j);
+}
+
+char	*ft_strtrim(const char *s1, char const *set)
+{
+	char	*str;
+	size_t	len;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	len = 0;
+	while (s1[len])
+		len++;
+	len = len - ft_trimlen(s1, set) + 1;
+	str = malloc(sizeof(char) * len);
+	while (ft_isinset(s1[i], set))
+		i++;
+	while (j < len)
+		str[j++] = s1[i++];
+	str[j] = 0;
 	return (str);
 }
